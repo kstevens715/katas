3787fe2e2046e7431f3980ac5cea7fa52e642b0b
Kyle Stevens <kstevens715@gmail.com>
2024-01-30 09:45:46 -0500

    Kyle's Solution

--- binary_tree_vertical_traversal/ruby/binary_tree.rb
+++ binary_tree_vertical_traversal/ruby/binary_tree.rb
@@ -2,10 +2,33 @@ class BinaryTree
 
   def initialize(root_node)
     @root_node = root_node
+    @results = {}
   end
 
   def vertical_traversal
-    # TODO
+    traverse root_node
+
+    output_results
   end
 
+  private
+
+  attr_reader :results, :root_node
+
+  def output_results
+    results.sort.map do |_, value|
+      value.values.flatten.sort
+    end
+  end
+
+  def traverse(node, x: 0, y: 0)
+    return unless node
+
+    results[x] ||= {}
+    results[x][y] ||= []
+    results[x][y] << node.value
+
+    traverse node.left, x: x - 1, y: y + 1
+    traverse node.right, x: x + 1, y: y + 1
+  end
 end
