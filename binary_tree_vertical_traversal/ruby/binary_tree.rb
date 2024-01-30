0e15d2d9e760f7ef99638cb403c3dec91c854e6d
Kyle Stevens <kstevens715@gmail.com>
2024-01-30 10:01:05 -0500

    Refactor Results into separate class

--- binary_tree_vertical_traversal/ruby/binary_tree.rb
+++ binary_tree_vertical_traversal/ruby/binary_tree.rb
@@ -1,32 +1,44 @@
 class BinaryTree
 
+  class Results
+    attr_reader :results
+
+    def initialize
+      @results = {}
+    end
+
+    def add(x:, y:, value:)
+      results[x] ||= {}
+      results[x][y] ||= []
+      results[x][y] << value
+    end
+
+    def to_a
+      results.sort.map do |_, value|
+        value.values.flatten.sort
+      end
+    end
+  end
+
   def initialize(root_node)
     @root_node = root_node
-    @results = {}
+    @results = Results.new
   end
 
   def vertical_traversal
     traverse root_node
 
-    output_results
+    results.to_a
   end
 
   private
 
   attr_reader :results, :root_node
 
-  def output_results
-    results.sort.map do |_, value|
-      value.values.flatten.sort
-    end
-  end
-
   def traverse(node, x: 0, y: 0)
     return unless node
 
-    results[x] ||= {}
-    results[x][y] ||= []
-    results[x][y] << node.value
+    results.add x:, y:, value: node.value
 
     traverse node.left, x: x - 1, y: y + 1
     traverse node.right, x: x + 1, y: y + 1
